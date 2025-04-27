2dc1882ef356c393f7485f8899cc433daa1bd63c
Ramakrishna G <eravi1702@gmail.com>
2025-04-27 16:50:51 +0530

    add deploy

--- deploy.R
+++ deploy.R
@@ -1,25 +1,27 @@
 
+# Load required package
+if (!require("rsconnect")) {
+  install.packages("rsconnect")
+  library(rsconnect)
+}
 
-# Load rsconnect package
-library(rsconnect)
-
-# Set your shinyapps.io account info
+# Set account info
 rsconnect::setAccountInfo(
   name = "psmlabs",
   token = "8FAB74BDBB46C9CA05D75B7102711773",
   secret = "9a6AwJZn3QacJOlVCdzWv6Ptj34mOsuIRE3OuNDI"
 )
 
+# Create necessary directory if it doesn't exist
+deploy_dir <- file.path("rsconnect", "shinyapps.io", "demand_forecasty")
+if (!dir.exists(deploy_dir)) {
+  dir.create(deploy_dir, recursive = TRUE)
+}
+
 # Deploy the app
 rsconnect::deployApp(
-  appDir = ".",           # Current directory where your app is
-  appName = "demand_forecasty", # Name for your app on shinyapps.io
+  appDir = ".",         
+  appName = "demand_forecasty",   
+  appTitle = "DF", 
   account = "psmlabs"
 )
-
-
-dir.create("rsconnect/shinyapps.io/psmlabs", recursive = TRUE, showWarnings = FALSE)
-
-
-# Optionally, print a success message
-cat("Deployment complete!")
