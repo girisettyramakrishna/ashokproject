773de509dfcc29108e5b3ce80687699daf5aad83
Ramakrishna G <eravi1702@gmail.com>
2025-04-27 16:56:28 +0530

    add deploy

--- deploy.R
+++ deploy.R
@@ -5,6 +5,10 @@ if (!require("rsconnect")) {
   library(rsconnect)
 }
 
+# Disable deployment record writing
+Sys.setenv(RSCONNECT_DEPLOYMENT_RECORD_DISABLE = "true")
+
+
 # Set account info
 rsconnect::setAccountInfo(
   name = "psmlabs",
@@ -12,11 +16,6 @@ rsconnect::setAccountInfo(
   secret = "9a6AwJZn3QacJOlVCdzWv6Ptj34mOsuIRE3OuNDI"
 )
 
-# Create necessary directory if it doesn't exist
-deploy_dir <- file.path("rsconnect", "shinyapps.io", "demand_forecasty")
-if (!dir.exists(deploy_dir)) {
-  dir.create(deploy_dir, recursive = TRUE)
-}
 
 # Deploy the app
 rsconnect::deployApp(
